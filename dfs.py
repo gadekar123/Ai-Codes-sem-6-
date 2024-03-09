9bab21f640d9b0c944000720551ce01142695a61
gadekar123 <gadekarshivam111@gmail.com>
2024-03-09 13:15:10 +0530

    dfs added

---
+++ dfs.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+graph = {
+    0: [1, 2],
+    1: [2, 3, 4],
+    2: [0, 3],
+    3: [1, 2, 4],
+    4: [1, 3]
+}
+
+def dfs(graph, start):
+  
+
+    visited = set()  # Keep track of visited vertices
+    queue = deque([start])  # Use a deque for efficient stack operations
+
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            visited.add(vertex)
+            print(vertex, end=" ")
+
+            for neighbor in graph[vertex]:
+                queue.append(neighbor)
+
+# Perform DFS traversal starting from vertex 0
+print("dfs traversal (starting from vertex 0):")
+dfs(graph, 0)
